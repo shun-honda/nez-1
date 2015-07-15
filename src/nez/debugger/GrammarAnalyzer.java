6143c4f76686fb875e26f9d47b2ea9c36806778d
shun-honda <shun.honda.suffix@gmail.com>
2015-07-15 15:59:47 +0900

    added unrechable choice analyzer

--- src/nez/debugger/GrammarAnalyzer.java
+++ src/nez/debugger/GrammarAnalyzer.java
@@ -1,11 +1,19 @@
 package nez.debugger;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import nez.lang.AnyChar;
+import nez.lang.ByteChar;
+import nez.lang.ByteMap;
 import nez.lang.Choice;
+import nez.lang.Empty;
 import nez.lang.Expression;
 import nez.lang.Failure;
 import nez.lang.Grammar;
 import nez.lang.GrammarOptimizer;
+import nez.lang.GrammarReshaper;
+import nez.lang.NonTerminal;
 import nez.lang.Not;
 import nez.lang.Option;
 import nez.lang.Production;
@@ -91,6 +99,104 @@ public class GrammarAnalyzer {
 			return true;
 		}
 		return true;
+	}
+
+}
+
+class UnreachableAnalyzer extends GrammarReshaper {
+	ArrayList<NonTerminal> recursiveList = new ArrayList<NonTerminal>();
+
+	class Consume {
+		boolean isChoiceElementConsumption = false;
+		Choice choice;
+		NonTerminal ne;
+		Consume prev;
+		List<Consume> next;
+		int index;
+		List<Integer> consumeList;
+
+		public Consume(Choice choice, int index, Consume prev) {
+			this.isChoiceElementConsumption = true;
+			this.choice = choice;
+			this.index = index;
+			this.prev = prev;
+			if(prev != null) {
+				this.prev.next.add(this);
+			}
+			this.next = new ArrayList<Consume>();
+			this.consumeList = new ArrayList<Integer>();
+		}
+
+		public Consume(NonTerminal ne) {
+			this.ne = ne;
+		}
+
+		public Consume append(int ch) {
+			this.consumeList.add(ch);
+			return this;
+		}
+
+		@Override
+		public String toString() {
+			if(this.isChoiceElementConsumption) {
+				return "(" + this.index + ")" + this.consumeList.toString();
+			}
+			return this.ne.getLocalName();
+		}
+	}
+
+	Consume cur;
+
+	public void pushConsume(Choice c, int index) {
+		this.cur = new Consume(c, index, this.cur);
+	}
+
+	public Consume popConsume() {
+		Consume c = this.cur;
+		this.cur = this.cur.prev;
+		return c;
+	}
+
+	public Consume peekConsume() {
+		return this.cur;
+	}
+
+	@Override
+	public Expression reshapeNonTerminal(NonTerminal p) {
+		NonTerminal ne = (NonTerminal) p;
+		boolean contains = recursiveList.contains(ne);
+		boolean isRecursive = ne.getProduction().isRecursive();
+		if(!contains && isRecursive) {
+			recursiveList.add(ne);
+		} else if(contains) {
+			return p;
+		}
+		return GrammarOptimizer.resolveNonTerminal(p).reshape(this);
+	}
+
+	@Override
+	public Expression reshapeEmpty(Empty e) {
+		return e;
+	}
+
+	@Override
+	public Expression reshapeFailure(Failure e) {
+		return e;
+	}
+
+	@Override
+	public Expression reshapeByteChar(ByteChar e) {
+		return e;
+	}
+
+	@Override
+	public Expression reshapeByteMap(ByteMap e) {
+		return e;
+	}
+
+	@Override
+	public Expression reshapeAnyChar(AnyChar e) {
+		return e;
 	}
 
 }
