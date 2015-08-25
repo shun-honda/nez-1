5e614db7c39c22b84de645fd0318f37fe61b16aa
shun-honda <shun.honda.suffix@gmail.com>
2015-08-25 12:33:31 +0900

    added MiniNez InstructionSet

---
+++ src/nez/mininez/InstructionSet.java
@@ -0,0 +1,19 @@
+package nez.mininez;
+
+public enum InstructionSet {
+	Iexit,
+	Inop,
+	Ifail,
+	Ialt,
+	Isucc,
+	Ijump,
+	Icall,
+	Iret,
+	Ipos,
+	Iback,
+	Iskip,
+	Ibyte,
+	Iany,
+	Istr,
+	Iset;
+}
