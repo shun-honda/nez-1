6496e76732108e7e418cf40d92c0dd2bbf59b047
shun-honda <shun.honda.suffix@gmail.com>
2015-12-02 18:10:41 +0900

    updated MacroBuilder

--- src/nez/lang/macro/MacroBuilder.java
+++ src/nez/lang/macro/MacroBuilder.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import nez.ast.Symbol;
 import nez.ast.Tree;
 import nez.lang.macro.MacroBuilder.DefaultVisitor;
+import nez.util.ConsoleUtils;
 import nez.util.VisitorMap;
 
 public class MacroBuilder extends VisitorMap<DefaultVisitor> {
@@ -19,15 +20,22 @@ public class MacroBuilder extends VisitorMap<DefaultVisitor> {
 		return node;
 	}
 
-	public NezMacro accept(Tree<?> node) {
+	public NezMacro visit(Tree<?> node) {
 		return find(node.getTag().getSymbol()).accept(node);
 	}
 
 	public final static Symbol _name = Symbol.tag("name");
 	public final static Symbol _param = Symbol.tag("param");
+	public final static Symbol _body = Symbol.tag("body");
+	public final static Symbol _val = Symbol.tag("val");
+	public final static Symbol _label = Symbol.tag("label");
 
 	public void addMacro(Tree<?> node) {
-		accept(node);
+		try {
+			visit(node);
+		} catch (Exception e) {
+			ConsoleUtils.exit(1, e.toString());
+		}
 	}
 
 	public class DefaultVisitor {
@@ -40,13 +48,45 @@ public class MacroBuilder extends VisitorMap<DefaultVisitor> {
 		@Override
 		public NezMacro accept(Tree<?> node) {
 			String name = node.getText(_name, null);
-			DesugarFunction func = new DesugarFunction(name, node);
+			DesugarFunction func = new DesugarFunction(name, node, visit(node.get(_body)));
 			Tree<?> params = node.get(_param);
 			for (Tree<?> param : params) {
 				func.addParam(new TransVariable(param.toText(), param));
 			}
-			System.out.println(node);
-			return null;
+			desugarFunctionMap.put(name, func);
+			return func;
+		}
+	}
+
+	public class Block extends DefaultVisitor {
+		@Override
+		public NezMacro accept(Tree<?> node) {
+			nez.lang.macro.Block block = new nez.lang.macro.Block(node);
+			for (Tree<?> child : node) {
+				block.add(visit(child));
+			}
+			return block;
+		}
+	}
+
+	public class NodeLiteral extends DefaultVisitor {
+		@Override
+		public NezMacro accept(Tree<?> node) {
+			nez.lang.macro.NodeLiteral nodeLiteral = new nez.lang.macro.NodeLiteral(node);
+			Tree<?> list = node.get(_val);
+			for (Tree<?> element : list) {
+
+			}
+			return nodeLiteral;
+		}
+	}
+
+	public class Element extends DefaultVisitor {
+		@Override
+		public NezMacro accept(Tree<?> node) {
+			NodeElement element = new NodeElement(node, Symbol.tag(node.get(_label).getText(_name, null)));
+
+			return element;
 		}
 	}
 
--- src/nez/lang/macro/NezMacro.java
+++ src/nez/lang/macro/NezMacro.java
@@ -3,27 +3,25 @@ package nez.lang.macro;
 import java.util.ArrayList;
 import java.util.List;
 
+import nez.ast.Symbol;
 import nez.ast.Tree;
 
 public class NezMacro {
-	String name;
 	Tree<?> node;
+	NezMacro child;
 
-	public NezMacro(String name, Tree<?> node) {
-		this.name = name;
+	public NezMacro(Tree<?> node, NezMacro macro) {
 		this.node = node;
-	}
-
-	public String getName() {
-		return this.name;
 	}
 }
 
 class DesugarFunction extends NezMacro {
+	String name;
 	List<NezMacro> params;
 
-	public DesugarFunction(String name, Tree<?> node) {
-		super(name, node);
+	public DesugarFunction(String name, Tree<?> node, NezMacro macro) {
+		super(node, macro);
+		this.name = name;
 		params = new ArrayList<>();
 	}
 
@@ -35,57 +33,110 @@ class DesugarFunction extends NezMacro {
 		this.params.add(index, macro);
 	}
 
+	public String getName() {
+		return this.name;
+	}
+
 }
 
 class TransFunction extends NezMacro {
+	String name;
 
-	public TransFunction(String name, Tree<?> node) {
-		super(name, node);
+	public TransFunction(String name, Tree<?> node, NezMacro macro) {
+		super(node, macro);
+		this.name = name;
+	}
+
+	public String getName() {
+		return this.name;
 	}
 
 }
 
 class TransVariable extends NezMacro {
+	String name;
 	Tree<?> desugarNode;
 
 	public TransVariable(String name, Tree<?> node) {
-		super(name, node);
+		super(node, null);
+		this.name = name;
 	}
 
 	public void setDesugarNode(Tree<?> node) {
 		this.desugarNode = node;
 	}
 
+	public String getName() {
+		return this.name;
+	}
+
 }
 
 class NodeLiteral extends NezMacro {
+	Tree<?> desugarNode;
 
-	public NodeLiteral(String name, Tree<?> node) {
-		super(name, node);
+	public NodeLiteral(Tree<?> node) {
+		super(node, null);
+	}
+
+	public void createDesugarTree(Tree<?> node, Class<?> clazz) throws InstantiationException, IllegalAccessException {
+		desugarNode = (Tree<?>) clazz.newInstance();
+	}
+
+}
+
+class NodeElement extends NezMacro {
+	Symbol desugarLabel;
+	Symbol nodeLabel;
+	int index;
+
+	public NodeElement(Tree<?> node, Symbol label) {
+		super(node, null);
+		this.desugarLabel = label;
+	}
+
+	public NodeElement(Tree<?> node, Symbol label, Symbol nodeLabel) {
+		this(node, label);
+		this.nodeLabel = nodeLabel;
+	}
+
+	public NodeElement(Tree<?> node, Symbol label, int index) {
+		this(node, label);
+		this.index = index;
 	}
 
 }
 
 class Block extends NezMacro {
+	List<NezMacro> list;
 
-	public Block(String name, Tree<?> node) {
-		super(name, node);
+	public Block(Tree<?> node) {
+		super(node, null);
+		this.list = new ArrayList<>();
+	}
+
+	public void add(NezMacro macro) {
+		this.list.add(macro);
+	}
+
+	public void set(int index, NezMacro macro) {
+		this.list.add(index, macro);
 	}
 
 }
 
 class ListParam extends NezMacro {
 
-	public ListParam(String name, Tree<?> node) {
-		super(name, node);
+	public ListParam(Tree<?> node, NezMacro macro) {
+		super(node, macro);
 	}
 
 }
 
 class EmptyList extends NezMacro {
 
-	public EmptyList(String name, Tree<?> node) {
-		super(name, node);
+	public EmptyList(Tree<?> node, NezMacro macro) {
+		super(node, macro);
 	}
 
 }
