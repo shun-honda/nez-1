dd2337edf8bc4a14ee1b05b3aedb68a9ac150bd9
shun-honda <shun.honda.suffix@gmail.com>
2016-01-28 17:57:03 +0900

    added Fomatter

---
+++ devel/nez/bx/Formatter.java
@@ -0,0 +1,24 @@
+package nez.bx;
+
+import nez.ast.Symbol;
+
+public class Formatter {
+
+	private static final Symbol Format = Symbol.unique("Format");
+	private static final Symbol Name = Symbol.unique("Name");
+	private static final Symbol List = Symbol.unique("List");
+	private static final Symbol Param = Symbol.unique("Param");
+	private static final Symbol TagParam = Symbol.unique("TagParam");
+	private static final Symbol ListParam = Symbol.unique("ListParam");
+	private static final Symbol Text = Symbol.unique("Text");
+	private static final Symbol Apply = Symbol.unique("Apply");
+	private static final Symbol SystemVariable = Symbol.unique("SystemVariable");
+	private static final Symbol Field = Symbol.unique("Field");
+
+	private static final Symbol name = Symbol.unique("name");
+	private static final Symbol param = Symbol.unique("param");
+	private static final Symbol body = Symbol.unique("body");
+	private static final Symbol label = Symbol.unique("label");
+	private static final Symbol tag = Symbol.unique("tag");
+	private static final Symbol list = Symbol.unique("list");
+}
