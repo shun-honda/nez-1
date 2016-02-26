0fe588be615f6c9c9ee77f53318000a52e6e1d1a
masanori-10 <s.masanori.vx@gmail.com>
2016-02-26 22:10:48 +0900

    fixed ASTChecker bug

--- devel/nez/bx/ASTEqualChecker.java
+++ devel/nez/bx/ASTEqualChecker.java
@@ -8,22 +8,51 @@ public class ASTEqualChecker {
 	}
 
 	public boolean isEqual(Tree a, Tree x) {
-		if (!a.getTag().equals(x.getTag())) {
+		if (a.getTag() != null && x.getTag() != null) {
+			if (!a.getTag().equals(x.getTag())) {
+				System.out.println(a);
+				System.out.println("==============================");
+				System.out.println(x);
+				return false;
+			}
+		} else if (!(a.getTag() == null && x.getTag() == null)) {
+			System.out.println(a);
+			System.out.println("==============================");
+			System.out.println(x);
 			return false;
 		}
 		if (a.size() != x.size()) {
+			System.out.println(a);
+			System.out.println("==============================");
+			System.out.println(x);
 			return false;
 		}
 		for (int i = 0; i < a.size(); i++) {
 			if (!isEqual(a.get(i), x.get(i))) {
+				System.out.println(a);
+				System.out.println("==============================");
+				System.out.println(x);
 				return false;
 			}
-			if (!a.getLabel(i).equals(x.getLabel(i))) {
+			if (a.getLabel(i) != null && x.getLabel(i) != null) {
+				if (!a.getLabel(i).equals(x.getLabel(i))) {
+					System.out.println(a);
+					System.out.println("==============================");
+					System.out.println(x);
+					return false;
+				}
+			} else if (!(a.getLabel(i) == null && x.getLabel(i) == null)) {
+				System.out.println(a);
+				System.out.println("==============================");
+				System.out.println(x);
 				return false;
 			}
 		}
 		if (a.size() == 0) {
 			if (!a.getValue().equals(x.getValue())) {
+				System.out.println(a);
+				System.out.println("==============================");
+				System.out.println(x);
 				return false;
 			}
 		}
